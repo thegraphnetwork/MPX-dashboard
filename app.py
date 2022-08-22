71fae354155ea995b244976a5990fb1889c2b301
fccoelho <fccoelho@gmail.com>
2022-08-22 08:51:21 -0300

    Added simple plot of weekly cases added sidebar with TGN logo

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+pd.options.plotting.backend = "plotly"
 
-st.title('Monkey Pox Evolution')
+TITLE = 'Monkey Pox Evolution'
+st.set_page_config(page_title=TITLE,
+                    page_icon=":chart:",
+                    layout="wide",
+                    initial_sidebar_state="auto",
+                    menu_items={
+                                'Get Help': 'https://www.thegraphnetwork.org',
+                                'Report a bug': "https://github.com/thegraphnetwork/MPX-dashboard/issues",
+     })
+st.sidebar.image('tgn.png')
+st.title(TITLE)
 
 DATE_COLUMN = 'date/time'
 LINELIST_URL = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/latest.csv'
 TS_URL = 'https://raw.githubusercontent.com/globaldothealth/monkeypox/main/timeseries-confirmed.csv'
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_data(nrows):
     ll_data = pd.read_csv(LINELIST_URL, nrows=nrows)
     ts_data = pd.read_csv(TS_URL, nrows=nrows)
@@ -22,8 +33,19 @@ data_load_state = st.text('Loading data...')
 linelist, timeseries = load_data(10000)
 data_load_state.text("Done!")
 
+for c in linelist.columns:
+    if c.startswith('Date_'):
+        linelist[c] = pd.to_datetime(linelist[c], errors='coerce')
+
+
 if st.checkbox('Show linelist table'):
     st.subheader('Linelist data')
     st.dataframe(linelist)
 
-
+country = st.selectbox('Select country', list(set(linelist.Country)))
+country_list = linelist[linelist.Country==country]
+try:
+    fig = country_list.set_index('Date_confirmation')['ID'].resample('W').count().plot()
+    st.plotly_chart(fig, use_container_width=True)
+except:
+    st.markdown('No cases reported so far.')
